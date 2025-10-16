b4f5ac829bcb5c6c52821cedda1c06adcd2e4c9c
paulinacarrascoo <a01803105@tec.mx>
2025-10-16 07:07:18 -0600

    Add files via upload

---
+++ S6_A01803105.py
@@ -0,0 +1,25 @@
+    #Exercise 1
+print("Hello World! My name is Paulina")
+#Exercise 2
+input("What is your name?")
+#Exercise 3
+print("cat dog frog")
+#Exercise 4
+print("2")
+print("4")
+print("6")
+#Exercise 5
+input("What is your adress?")
+#Exercise 6
+print ("Whispers of Change")
+print ("The leaf may fall, yet roots still stay,")
+print ("The night must end to birth the day.")
+print ("What seems an end is just a start,")
+print ("A hidden strength within the heart.")
+#Exercise 7
+input ("Did you liked the poem?")
+#Exercise 8
+print("See you soon!! :)")
+
+print (3**2+4**2)
+print(A=1/4+5(5+2**0.5+5**0.5)+a**2)
